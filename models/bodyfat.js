1e24150a2bc927d9910e2d7d137c1b5030a9545d
kurtdonaldson <89885417+kurtdonaldson@users.noreply.github.com>
2022-07-12 21:08:31 +1200

    bodyfat changes

---
+++ models/bodyfat.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+// creating the variable Schema makes a shortcut
+const Schema = mongoose.Schema;
+
+const BodyfatSchema = new Schema({
+    name: String,
+    date: String,
+    leanMass: String,
+    weight: String,
+})
